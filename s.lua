a56c5d6494f783fc43d13cbefbc5f68f23a9a327
Trident_God <142750270+TridentBOY@users.noreply.github.com>
2025-04-08 21:16:47 -0400

    Update s.lua

--- s.lua
+++ s.lua
@@ -1,9 +1,18 @@
+local ProjectileDrop = function()
+    local mod = getgenv().modules.FPS.GetEquippedItem()
+    if not mod or not mod.id then
+        return 0, 0
+    end
+    local itemConfig = require(game:GetService("ReplicatedStorage").ItemConfigs[mod.id])
+    return mod.id, itemConfig.ProjectileSpeed, itemConfig.ProjectileDrop
+end
+
 local oldfromOrientation; oldfromOrientation = hookfunction(CFrame.fromOrientation, newcclosure(function(p, y, r)
     if getgenv().VisualsWTFConfig.silent and getgenv().SilentTarget and debug.info(3, "n") == "fire" then
         local id, speed, drop = ProjectileDrop()
         local targetHead = getgenv().SilentTarget.model.Head
         if targetHead then
-            local origin = modules.Camera.GetCFrame()
+            local origin = getgenv().modules.Camera.GetCFrame()
             local Camera = game.workspace.CurrentCamera
             local cameraPosition = Camera.CFrame.Position
             local predictedPos = PerfectPrediction(origin.Position, targetHead.Position, speed, drop)
